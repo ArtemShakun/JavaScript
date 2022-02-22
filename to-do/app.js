a20230b373db108a1d9c33ec416e249fd73fc95c
artemshakun <artem.shakun.91@gmail.com>
2022-02-22 10:52:33 +0200

    created functions releated with server

--- to-do/app.js
+++ to-do/app.js
@@ -1,3 +1,17 @@
+// Globals
+let todos = [];
+let users = [];
+
+// Attach event
+document.addEventListener('DOMContentLoaded', initApp);
+
+// Event logic
+function initApp() {
+    Promise.all([getAllToDos(), getAllUsers()]).then(value => {
+        [todos, users] = value;
+    })
+}
+
 // Async logic
 async function getAllToDos() {
     const response = await fetch('https://jsonplaceholder.typicode.com/todos');
